cc2493af8529f43442474b26d941f772bbaa3b05
Pollot <olaf.moroz.mirtex@gmail.com>
2023-11-19 03:25:07 +0100

    Printing improvements and volume bar added

--- spotify-volume.py
+++ spotify-volume.py
@@ -6,6 +6,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.exceptions import SpotifyException
 import keyboard
+import sys
 
 # Keybinds
 volume_up_key = "f13"
@@ -17,6 +18,7 @@ exit_key = "f12"
 volume_step = 2   # The increment/decrement step size for volume adjustments
 max_volume = 100  # Maximum playback volume
 min_volume = 0    # Minimum playback volume
+bar_length = 25   # Volume bar length
 refresh_rate = 10 # Time in seconds to refresh playback data
 
 load_dotenv()
@@ -31,6 +33,26 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secre
 
 def clear_terminal():
     os.system('cls' if os.name=='nt' else 'clear')
+
+# Prints in the same line
+last_length = 0
+def print_inline(string):
+    global last_length
+    current_length = len(string)
+    print(f"\r{string}", end=" " * (last_length - current_length), flush=True)
+    last_length = len(string)
+
+# Volume bar
+def print_volume(volume):
+    filled_length = int(bar_length * volume / max_volume)
+    bar = "#" * filled_length + "-" * (bar_length - filled_length)
+    print_inline(f"Volume: [{bar}] {volume}%")
+
+def print_muted():
+    print_inline("Playback is muted")
+
+def print_blocked():
+    print_inline("Can't change volume becaue nothing is currently playing")
 
 volume = 0 # Initial volume, it will change after playback has been started
 block_keys = False # It will block keys, unless there is a song playing
@@ -62,7 +84,7 @@ def refresh_playback_data():
                     sp.volume(volume)
                 except SpotifyException as exception:
                     pass  
-                print(f"Playback volume: {volume}%")
+                print_volume(volume)
             
             block_keys = False
 
@@ -84,67 +106,68 @@ muted = False
 
 def volume_up(key):
     if muted:
-        print("Playback is muted")
         return
     
     if block_keys:
-        print("Can't change volume becaue nothing is currently playing")
+        print_blocked()
         return
     
     keyboard.block_key(volume_up_key)
     global volume
+
     try:
         if volume <= max_volume - volume_step:
             volume += volume_step
             sp.volume(volume)
-            print(f"Volume increased to {volume}%")
-        else:
-            print(f"Volume is already at {volume}%")
-
+            print_volume(volume)
     except SpotifyException as exception:
         pass
+
     keyboard.unblock_key(volume_up_key)
+
 
 def volume_down(key):
     if muted:
-        print("Playback is muted")
         return
     
     if block_keys:
-        print("Can't change volume becaue nothing is currently playing")
+        print_blocked()
         return
 
     keyboard.block_key(volume_down_key)
     global volume
+
     try:
         if volume >= min_volume + volume_step:
             volume -= volume_step
             sp.volume(volume)
-            print(f"Volume decreased to {volume}%")
-        else:
-            print(f"Volume is already at {volume}%")
+            print_volume(volume)
     except SpotifyException as exception:
         pass
+
     keyboard.unblock_key(volume_down_key)
+
 
 def volume_mute(key):
     if block_keys:
-        print("Can't change volume becaue nothing is currently playing")
+        print_blocked()
         return
     
     keyboard.block_key(volume_mute_key)
     global muted
+
     try:
         if muted:
             sp.volume(volume)
             muted = False
-            print("Playback unmuted")
+            print_volume(volume)
         else:
             sp.volume(0)
             muted = True
-            print("Playback muted")
+            print_muted()
     except SpotifyException as exception:
         pass
+    
     keyboard.unblock_key(volume_mute_key)
 
 
@@ -153,4 +176,4 @@ keyboard.on_press_key(volume_down_key, volume_down)
 keyboard.on_press_key(volume_mute_key, volume_mute)
 
 keyboard.wait(exit_key)
-print("Exiting the program...")
+print_inline("Exiting the program...")
